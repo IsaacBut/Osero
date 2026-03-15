bb50b7220b430a814672864eef38c87a49ca52c6
ヒツタンコウ <Butchamhong@gmail.com>
2026-03-16 08:26:55 +0900

    全てインデックスに追加

---
+++ GameBase/targetver.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+// // SDKDDKVer.h をインクルードすると、利用できる最も高いレベルの Windows プラットフォームが定義されます。
+// 以前の Windows プラットフォーム用にアプリケーションをビルドする場合は、WinSDKVer.h をインクルードし、
+// サポートしたいプラットフォームに _WIN
+// 2_WINNT マクロを設定してから SDKDDKVer.h をインクルードします。
+#include <SDKDDKVer.h>
